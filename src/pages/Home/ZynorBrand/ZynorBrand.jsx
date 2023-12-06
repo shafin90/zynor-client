4a283640f524e0de4ad7995c95fea268301570fc
Mashrafi Ahnam Shafin <132218231+shafin90@users.noreply.github.com>
2023-12-06 20:20:28 +0600

    Update ZynorBrand.jsx
    push

--- src/pages/Home/ZynorBrand/ZynorBrand.jsx
+++ src/pages/Home/ZynorBrand/ZynorBrand.jsx
@@ -6,7 +6,7 @@ const ZynorBrand = () => {
         <div className=" position-relative zynor-brand  p-5   d-flex flex-column justify-content-center align-items-center">
 
             <div className="z-1 pt-5 pb-3  d-flex justify-content-center align-items-center">
-                <img className="w-25" src="../../../../public/assets/logo.png" alt="" />
+                <img className="w-25" src="/assets/logo.png" alt="" />
                 <h1 className="text-white zynor-heading mb-0" style={{fontFamily:"battambang"}}>
                     ZYNOR
                 </h1>
