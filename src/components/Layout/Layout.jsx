2ec6685ef9713d8a522d6ebb252b996852f2eb5a
shafin90 <mashrafiahnam1@gmail.com>
2023-12-07 19:28:03 -0800

    push

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -8,15 +8,9 @@ import FindYourWork from "../../pages/Home/FindYourWork/FindYourWork";
 import ClientsOpinion from "../../pages/Home/ClientsOpinion/ClientsOpinion";
 import FAQ from "../../pages/Home/FAQ/FAQ";
 import ZynorBrand from "../../pages/Home/ZynorBrand/ZynorBrand";
-<<<<<<< HEAD
+
 import GapInHome from "../../pages/Home/gapInHome/gapInHome";
 import { Outlet } from "react-router-dom";
-=======
-import GapInHome from "../../pages/Home/GapInHome/GapInHome";
-
-
-
->>>>>>> 08a7119424d79dce826aaf99728d889a994a63d1
 
 
 const Layout = () => {
