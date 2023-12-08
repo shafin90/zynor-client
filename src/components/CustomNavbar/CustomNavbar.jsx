b79b11f40954705318017aaddc8c3d002f255250
shafin90 <mashrafiahnam1@gmail.com>
2023-12-07 19:45:24 -0800

    push

--- src/components/CustomNavbar/CustomNavbar.jsx
+++ src/components/CustomNavbar/CustomNavbar.jsx
@@ -16,18 +16,20 @@ const CustomNavbar = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mx-auto">
-                        <Nav.Link style={{fontFamily:"Nunito"}} as={Link} to="services" className=' text-white' >Our Service</Nav.Link>
-                        
-                        <Nav.Link style={{fontFamily:"Nunito"}} className=' text-white' href="#works">Our Works</Nav.Link>
-                        
-                        <Nav.Link style={{fontFamily:"Nunito"}} className=' text-white' href="#blog">Our Blog</Nav.Link>
-                        
-                        <Nav.Link style={{fontFamily:"Nunito"}} className=' text-white' href="#content">Our Content</Nav.Link>
+                        <Nav.Link style={{ fontFamily: "Nunito" }} as={Link} to="/" className='text-white' >Home</Nav.Link>
+
+                        <Nav.Link style={{ fontFamily: "Nunito" }} as={Link} to="services" className=' text-white' >Our Service</Nav.Link>
+
+                        <Nav.Link style={{ fontFamily: "Nunito" }} className=' text-white' href="#works">Our Works</Nav.Link>
+
+                        <Nav.Link style={{ fontFamily: "Nunito" }} className=' text-white' href="#blog">Our Blog</Nav.Link>
+
+                        <Nav.Link style={{ fontFamily: "Nunito" }} className=' text-white' href="#content">Our Content</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
 
                 {/* Right side - Button */}
-                <button style={{fontFamily:"Nunito"}} className='btn-pink text-white border-0 px-4 py-3 rounded-4 fw-bold'>We Are Hiring</button>
+                <button style={{ fontFamily: "Nunito" }} className='btn-pink text-white border-0 px-4 py-3 rounded-4 fw-bold'>We Are Hiring</button>
             </Container>
         </Navbar>
     );
