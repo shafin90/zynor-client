3c7fd8408b4475a2454fab0be6f26e02bb674a45
shafin90 <mashrafiahnam1@gmail.com>
2023-12-10 23:04:41 +0600

    push

--- src/pages/OurBlog/OurBlog.jsx
+++ src/pages/OurBlog/OurBlog.jsx
@@ -10,7 +10,7 @@ const OurBlog = () => {
                     <OurBlogCard
                         heading='Set Free the Power of Digital Marketing'
                         article='Your Gateway to Online Success'
-                        img="../../../public/assets/Rectangle 13.jpg"
+                        img="/assets/Rectangle 13.jpg"
                     ></OurBlogCard>
                 </Col>
 
@@ -18,7 +18,7 @@ const OurBlog = () => {
                     <OurBlogCard
                         heading='Social Media Marketing'
                         article='Building Your Brand Online'
-                        img="../../../public/assets/Rectangle 12.jpg"
+                        img="/assets/Rectangle 12.jpg"
                     ></OurBlogCard>
                 </Col>
 
@@ -26,7 +26,7 @@ const OurBlog = () => {
                     <OurBlogCard
                         heading='Email Marketing'
                         article='Building and Nurturing Your Online Community'
-                        img="../../../public/assets/Rectangle 14.jpg"
+                        img="/assets/Rectangle 14.jpg"
                     ></OurBlogCard>
                 </Col>
             </Row>
@@ -37,7 +37,7 @@ const OurBlog = () => {
                     <OurBlogCard
                         heading='Content Marketing Mastery'
                         article='Creating Value that Drives Results'
-                        img="../../../public/assets/Rectangle 16.jpg"
+                        img="/assets/Rectangle 16.jpg"
                     ></OurBlogCard>
                 </Col>
 
@@ -45,7 +45,7 @@ const OurBlog = () => {
                     <OurBlogCard
                         heading='Evolving Customer Experience'
                         article='Personalization and Engagement in the Digital Age'
-                        img="../../../public/assets/Rectangle 15.jpg"
+                        img="/assets/Rectangle 15.jpg"
                     ></OurBlogCard>
                 </Col>
 
@@ -53,7 +53,7 @@ const OurBlog = () => {
                     <OurBlogCard
                         heading='How to Create a Killer SEO Strategy'
                         article='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor malesuada sem ultricies tristique.'
-                        img="../../../public/assets/Rectangle 17.jpg"
+                        img="/assets/Rectangle 17.jpg"
                     ></OurBlogCard>
                 </Col>
             </Row>
@@ -65,7 +65,7 @@ const OurBlog = () => {
                     <OurBlogCard
                         heading='Navigating the Social Media Landscape'
                         article='A Digital Marketing Guide'
-                        img="../../../public/assets/Rectangle 13 (1).jpg"
+                        img="/assets/Rectangle 13 (1).jpg"
                     ></OurBlogCard>
                 </Col>
 
@@ -73,7 +73,7 @@ const OurBlog = () => {
                     <OurBlogCard
                         heading='Embracing Micro-Influencers'
                         article='Leveraging Authenticity and Niche Communities for Targeted Marketing'
-                        img="../../../public/assets/Rectangle 12 (1).jpg"
+                        img="/assets/Rectangle 12 (1).jpg"
                     ></OurBlogCard>
                 </Col>
 
@@ -81,15 +81,15 @@ const OurBlog = () => {
                     <OurBlogCard
                         heading='The Future is Exciting'
                         article='Embracing the Evolution of Digital Marketing'
-                        img="../../../public/assets/Rectangle 14 (1).jpg"
+                        img="/assets/Rectangle 14 (1).jpg"
                     ></OurBlogCard>
                 </Col>
             </Row>
 
-<div className=' my-5 py-5'></div>
+            <div className=' my-5 py-5'></div>
 
             <div className=' d-flex flex-column justify-content-between align-items-center my-5 py-5 px-5'>
-                <h1 style={{fontFamily:"battambang"}} className='text-white text-center mb-3 fw-bold'>
+                <h1 style={{ fontFamily: "battambang" }} className='text-white text-center mb-3 fw-bold'>
                     Lorem ipsum dolor sit amet
                 </h1>
                 <article className='text-white text-center mb-5 px-5'>
